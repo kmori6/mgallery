255a06056718e25d244dbafc1f4397a42eabf825
morik <kosuke.mori.5120@gmail.com>
2022-12-29 10:29:52 +0900

    update metrics

--- metrics.py
+++ metrics.py
@@ -1,5 +1,20 @@
 import numpy as np
 
 
-def mean_squared_error(preds: np.ndarray, targets: np.ndarray) -> float:
-    return np.mean((targets - preds) ** 2)
+class Metrics:
+    def __init__(self, metcis: str):
+        self.metrics = metcis
+
+    def __call__(self, preds: np.ndarray, targets: np.ndarray):
+        if self.metrics == "mse":
+            return self.mean_squared_error(preds, targets)
+        elif self.metrics == "acc":
+            return self.accuracy(preds, targets)
+
+    @staticmethod
+    def mean_squared_error(preds: np.ndarray, targets: np.ndarray) -> float:
+        return np.mean((targets - preds) ** 2)
+
+    @staticmethod
+    def accuracy(preds: np.ndarray, targets: np.ndarray) -> float:
+        return (preds == targets).sum() / len(targets)
